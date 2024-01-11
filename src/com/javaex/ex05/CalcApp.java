288304fdc8e6af57c6811248ab74ee5a971119f9
aassdd236 <aassdd236@naver.com>
2024-01-11 18:35:00 +0900

    5

--- src/com/javaex/ex05/CalcApp.java
+++ src/com/javaex/ex05/CalcApp.java
@@ -12,33 +12,40 @@ public class CalcApp {
 		do{
 			System.out.print(">> ");
 			str=s.nextLine();
-			sArray=str.split(" ");
 
-			switch(sArray[1]) {
-			case "+":
-				Add add=new Add();
-				add.setValue(Integer.parseInt(sArray[0]), Integer.parseInt(sArray[2]));
-				System.out.println(add.calculate());
+			if ("/q".equals(str)) {
+				System.out.println("종료합니다.");
 				break;
-			case "-":
-				Sub sub=new Sub();
-				sub.setValue(Integer.parseInt(sArray[0]), Integer.parseInt(sArray[2]));
-				System.out.println(sub.calculate());
-				break;
-			case "*":
-				Mul mul=new Mul();
-				mul.setValue(Integer.parseInt(sArray[0]), Integer.parseInt(sArray[2]));
-				System.out.println(mul.calculate());
-				break;
-			case "/":
-				Div div=new Div();
-				div.setValue(Integer.parseInt(sArray[0]), Integer.parseInt(sArray[2]));
-				System.out.println(div.calculate());
-				break;
-			default:
-				System.out.println("알 수 없는 연산자입니다.");
 			}
-		}while(str!="/q");
-		System.out.println("종료합니다.");
+
+			sArray=str.split(" ");
+			if (sArray.length == 3) {
+				switch(sArray[1]) {
+				case "+":
+					Add add=new Add();
+					add.setValue(Integer.parseInt(sArray[0]), Integer.parseInt(sArray[2]));
+					System.out.println(add.calculate());
+					break;
+				case "-":
+					Sub sub=new Sub();
+					sub.setValue(Integer.parseInt(sArray[0]), Integer.parseInt(sArray[2]));
+					System.out.println(sub.calculate());
+					break;
+				case "*":
+					Mul mul=new Mul();
+					mul.setValue(Integer.parseInt(sArray[0]), Integer.parseInt(sArray[2]));
+					System.out.println(mul.calculate());
+					break;
+				case "/":
+					Div div=new Div();
+					div.setValue(Integer.parseInt(sArray[0]), Integer.parseInt(sArray[2]));
+					System.out.println(div.calculate());
+					break;
+				default:
+					System.out.println("알 수 없는 연산자입니다.");
+				}
+			}
+		}while(true);
+
 	}
 }
