31af641d79b9a5e177108ac7d82521e89cd58a08
aassdd236 <aassdd236@naver.com>
2024-01-12 09:47:21 +0900

    5

--- src/com/javaex/ex05/CalcApp.java
+++ src/com/javaex/ex05/CalcApp.java
@@ -9,7 +9,7 @@ public class CalcApp {
 		String str;
 		String[] sArray;
 
-		do{
+		while(true) {
 			System.out.print(">> ");
 			str=s.nextLine();
 
@@ -24,28 +24,28 @@ public class CalcApp {
 				case "+":
 					Add add=new Add();
 					add.setValue(Integer.parseInt(sArray[0]), Integer.parseInt(sArray[2]));
-					System.out.println(add.calculate());
+					System.out.println(">>"+add.calculate());
 					break;
 				case "-":
 					Sub sub=new Sub();
 					sub.setValue(Integer.parseInt(sArray[0]), Integer.parseInt(sArray[2]));
-					System.out.println(sub.calculate());
+					System.out.println(">>"+sub.calculate());
 					break;
 				case "*":
 					Mul mul=new Mul();
 					mul.setValue(Integer.parseInt(sArray[0]), Integer.parseInt(sArray[2]));
-					System.out.println(mul.calculate());
+					System.out.println(">>"+mul.calculate());
 					break;
 				case "/":
 					Div div=new Div();
 					div.setValue(Integer.parseInt(sArray[0]), Integer.parseInt(sArray[2]));
-					System.out.println(div.calculate());
+					System.out.println(">>"+div.calculate());
 					break;
 				default:
 					System.out.println("알 수 없는 연산자입니다.");
 				}
 			}
-		}while(true);
+		}
 
 	}
 }
